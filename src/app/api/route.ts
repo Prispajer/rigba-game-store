29f0999aa760934101d3e868490ded94381eb279
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2024-02-10 00:18:36 +0100

    Trying to display elements on div

---
+++ src/app/api/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { query } from "../../data/database";
+
+export async function GET() {
+  try {
+    const results = await query({ query: "SELECT * FROM Users" });
+    return NextResponse.json({
+      query: results,
+    });
+  } catch (error) {
+    throw new Error("Wystąpił błąd przy pobieraniu danych z bazy danych.");
+  }
+}
--- src/components/Interface/Home/HomeContainer.tsx
+++ src/components/Interface/Home/HomeContainer.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import { GET } from "@/app/api/route";
 
 export default function HomeContainer() {
   const [userData, setUserData] = React.useState([]);
@@ -14,31 +15,10 @@ export default function HomeContainer() {
     userLastLogin: Date;
   };
 
-  React.useEffect(() => {
-    fetchData();
-  });
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/api/route");
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      const data = await response.json();
-      setUserData(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <>
       <main className="bg-[#296CA6] w-full h-full">
-        <div className="flex items-center justify-center">
-          {userData.map((user: Users, index: number) => (
-            <li key={index}>{user.userId}</li>
-          ))}
-        </div>
+        <div className="flex items-center justify-center"></div>
       </main>
     </>
   );

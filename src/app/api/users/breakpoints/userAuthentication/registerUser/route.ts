0f7528c20bcfef1fe02e774f1a0c4ab5a1f334e2
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2024-03-18 22:15:26 +0100

    Setting up middleware and auth

--- src/app/api/users/breakpoints/userAuthentication/registerUser/route.ts
+++ src/app/api/users/breakpoints/userAuthentication/registerUser/route.ts
@@ -1,20 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { queryRequests } from "@/data/database/resources/users";
+import bcrypt from "bcrypt";
 
 export async function POST(request: NextRequest, response: NextResponse) {
   try {
+    // TODO HASH PASSWORD
     const userBody = await request.json();
     const { email, password } = userBody;
+    const hashedPassword = await bcrypt.hash(password, 10);
     const userExists = await queryRequests.getUser(email, password);
-
     if (userExists.length > 0) {
       return NextResponse.json({
-        error: "User with provided email exists",
+        error: "Użytkownik z podanym adresem email istnieje!",
       });
     } else {
       await queryRequests.registerUser(userBody);
       return NextResponse.json({
-        success: "User has been created",
+        success: "Email został wysłany!",
       });
     }
   } catch (error) {

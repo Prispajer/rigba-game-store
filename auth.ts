c3ae0f07c9564e68ccbbdac456bc2686071e8834
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2024-03-22 22:50:43 +0100

    Trying to connect neon tech

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import authConfig from "@/../../auth.config";
+import { TypeORMAdapter } from "@auth/typeorm-adapter";
 
 export const {
   handlers: { GET, POST },
@@ -7,6 +8,9 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  adapter: TypeORMAdapter(
+    "postgresql://e-commerce-shop_owner:************@ep-restless-bird-a2xo3kwx-pooler.eu-central-1.aws.neon.tech/e-commerce-shop?sslmode=require"
+  ),
   session: { strategy: "jwt" },
   ...authConfig,
 });
--- src/data/database/connection.ts
+++ src/data/database/connection.ts
@@ -1,10 +1,24 @@
 import mysql from "mysql2/promise";
+import postgres from "postgres";
 
-const dataBase = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const conn = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
 });
 
-export default dataBase;
+// const dataBase = mysql.createPool({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+// });
+
+// export default dataBase;
+
+export default conn;

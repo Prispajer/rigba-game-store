95390de1bef7c3435df461bb1b6b398ff6ea0d2e
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2024-03-25 23:55:35 +0100

    Trying to fix bug where page doesn't redirect

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,8 +2,10 @@ import LoginContainer from "@/components/auth/LoginContainer";
 import { auth } from "../../../../auth";
 
 export default async function Login() {
+  const session = await auth();
   return (
     <>
+      {JSON.stringify(session)}
       <LoginContainer />
     </>
   );

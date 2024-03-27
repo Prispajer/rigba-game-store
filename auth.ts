43e24c42bb88abcd08b99c737fbf114357387e04
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2024-03-27 23:57:50 +0100

    Added  USER role to the data base, token jwt and session jwt

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import authConfig from "@/../../auth.config";
+import { getUserById } from "@/data/database/publicSQL/queries";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { postgres } from "@/data/database/publicSQL/postgres";
 
@@ -9,6 +10,32 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(postgres),
   session: { strategy: "jwt" },
   ...authConfig,
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,22 @@
 import LoginContainer from "@/components/auth/LoginContainer";
-import { auth } from "../../../../auth";
+import { auth, signOut } from "../../../../auth";
 
 export default async function Login() {
   const session = await auth();
+  console.log(session);
   return (
     <>
       {JSON.stringify(session)}
       <LoginContainer />
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
     </>
   );
 }
